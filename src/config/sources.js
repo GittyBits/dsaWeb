8718ea082b7a22d945f0d80ba3e5d401b672cf3c
GittyBits <imailtausif@gmail.com>
2025-06-16 18:06:10 +0530

    Create sources.js

---
+++ src/config/sources.js
@@ -0,0 +1,15 @@
+import shraddha from "../data/shraddha.json";
+import lovebabbar from "../data/lovebabbar.json";
+import shraddhaImg from "../assets/shraddha.png";
+import lovebabbarImg from "../assets/lovebabbar.png";
+
+export default {
+  "Shradha Ma'am": {
+    data: shraddha,
+    img: shraddhaImg,
+  },
+  "Love Babbar": {
+    data: lovebabbar,
+    img: lovebabbarImg,
+  },
+};
