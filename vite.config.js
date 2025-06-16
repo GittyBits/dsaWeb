f48731a89b6076ba309aecc38d65ea9da75846b2
GittyBits <imailtausif@gmail.com>
2025-06-16 18:10:00 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@data': path.resolve(__dirname, 'src/data'),
+      "@data": path.resolve(__dirname, "./src/data"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@components": path.resolve(__dirname, "./src/components"),
     },
   },
 });
